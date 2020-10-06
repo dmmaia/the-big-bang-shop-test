5981c9a68639103fd3bc1910926cef3c15920675
dmmaia <david_mmaia@hotmail.com>
2020-10-06 00:37:54 -0300

    Home structure created

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import './styles.css';
+import ApresentationImage from '../../assets/apresentation-image-home.png'
 
 function Home() {
-  return <div />;
+  return (
+      <div className="container">
+          <div className="apresentation-box" >
+            
+            <div />
+          </div>
+      </div>
+  );
 }
 
 export default Home;
