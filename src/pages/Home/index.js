2f8b28a0875c12231ad6656a1529b1eba6d09cd5
dmmaia <david_mmaia@hotmail.com>
2020-10-06 22:43:13 -0300

    Mobile style changed

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 
 import './styles.css';
-import ApresentationImage from '../../assets/apresentation-image-home.png'
+import ApresentationImage from '../../assets/apresentation-image-home.png';
+import ApresentationImageMobile from '../../assets/apresentation-image-home-mobile.png'
 
 function Home() {
   return (
       <div className="container">
-          <div className="apresentation-box" >
-            
+            <div className="apresentation-box" >
+                <img alt="apresentation" src={ApresentationImage} className="desktop-apresentation-image"/>
+                <img alt="apresentation" src={ApresentationImageMobile} className="mobile-apresentation-image"/>
+                <div>
+                    <h2>Lorem ipsum dolor sit amet</h2>
+                    <p>Lorem ipsum dolor sit amet</p>
+                </div>
             <div />
           </div>
       </div>
