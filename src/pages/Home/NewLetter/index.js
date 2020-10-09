7ff63cf8a29dc30eb53cf7ac2b096da086024fb8
dmmaia <david_mmaia@hotmail.com>
2020-10-08 23:46:52 -0300

    Newletter box created

---
+++ src/pages/Home/NewLetter/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import "./styles.css";
+
+const NewLetter = () => (
+    <div className="newsLetter">
+        <h2>Namah</h2>
+        <h2>no seu e-mail</h2>
+        <p>Assine nossa newsletter</p>
+
+        <input type='text' placeholder="Seu e-mail" className="textEmail" />
+        <button>ASSINAR</button>
+
+        <p className="privacityTerms">
+            Ao clicar em “assinar”, você concorda em receber
+            e e-mails do Espaço Namah a aceita nossos Termos de Uso
+            e nossas Políticas de Privacidade.
+        </p>
+    </div>
+);
+
+export default NewLetter;
